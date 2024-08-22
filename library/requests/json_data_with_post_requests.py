0e6735830c79c3175016adc5422ce9e9def83c2b
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-22 17:52:41 +0300

    Create json_data_with_post_requests.py

---
+++ library/requests/json_data_with_post_requests.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+url = 'https://httpbin.org/post'
+headers = {'Content-Type': 'application/json'}
+json_data = {'key': 'value'}
+response = requests.post(url, headers=headers, data=json.dumps(json_data))
+print(response.status_code)
+print(response.json())
