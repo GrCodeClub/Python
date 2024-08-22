d450197a0865703d5cd771f6e512575591ef9e5c
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-22 18:28:08 +0300

    Delete library/requests/post_requests.py

--- library/requests/post_requests.py
+++
@@ -1,8 +0,0 @@
-# Απλό POST αίτημα
-import requests
-
-url = 'https://grcodeclub.gr/post'
-data = {'key': 'value'}
-response = requests.post(url, data=data)
-print(response.status_code)
-print(response.json())
