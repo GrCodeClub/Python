dc08475c72efca6a2e1e8cfc5a54e560b72c1859
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-22 17:54:16 +0300

    Update get_requests_with_params.py

--- library/requests/get_requests_with_params.py
+++ library/requests/get_requests_with_params.py
@@ -1,6 +1,6 @@
 import requests
 
 params = {'param1': 'value1', 'param2': 'value2'}
-response = requests.get('https://httpbin.org/get', params=params)
+response = requests.get('https://grcodeclub.gr/get', params=params)
 print(response.status_code)
 print(response.json())
